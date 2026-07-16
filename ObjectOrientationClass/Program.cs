a9ed24535bde8348f7174dbdab045b7219f40ab2
BarbieSweetDreamz <barbiesweetdreamz@gmail.com>
2026-07-16 15:43:42 +0200

    Object orientation class exercise

---
+++ ObjectOrientationClass/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectOrientationClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
